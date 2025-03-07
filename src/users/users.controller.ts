b25c493bade918a4abeec4d2e11298e57fdcbf25
Vini <viniciusdemourarosa@gmail.com>
2025-03-06 22:02:37 -0300

    feat: add signup method in users controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller()
+export class UsersController {
+  @Post('signup')
+  signUp(): string {
+    return 'This action register a new user';
+  }
+}
